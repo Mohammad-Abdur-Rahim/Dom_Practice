81260fc544b7dd036df1f6f2e7a36b0de64e037b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-14 15:09:02 +0600

    *** Dom CreateElement appendChild() ***

---
+++ basic dom/append.js
@@ -0,0 +1,5 @@
+const placelist=document.getElementById('place-list');
+const li = document.createElement('li');
+li.innerText='Gazipur my 2nd Home';
+// now list code add parent node--------
+placelist.appendChild(li);
