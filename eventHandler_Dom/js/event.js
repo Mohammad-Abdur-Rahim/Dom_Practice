90518da6c04d8918b78017e36ded88ca671da5f2
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-14 17:23:49 +0600

    *** Event addEventListner () ***

--- eventHandler_Dom/js/event.js
+++ eventHandler_Dom/js/event.js
@@ -1,7 +1,13 @@
 function makered(){
     document.body.style.backgroundColor='red';
 }
+function makePink(){
+    document.body.style.backgroundColor='pink';
+}
 const purple=document.getElementById('purple');
 purple.onclick=function purple(){
     document.body.style.backgroundColor='purple';
 }
+//addEventListner ------
+const pink=document.getElementById('pink');
+pink.addEventListener('click',makePink);
