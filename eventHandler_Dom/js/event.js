a901b2de65a73b73f2c1b561454495fcdf1b80b4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-14 17:15:16 +0600

    *** Event onclick() with getElemntById FN ***

--- eventHandler_Dom/js/event.js
+++ eventHandler_Dom/js/event.js
@@ -1,3 +1,7 @@
 function makered(){
     document.body.style.backgroundColor='red';
 }
+const purple=document.getElementById('purple');
+purple.onclick=function purple(){
+    document.body.style.backgroundColor='purple';
+}
