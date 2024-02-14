26db94e7d5e608593bd5c422a18305ecc86d2bd4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-14 17:40:15 +0600

    *** Event addEventListner () final use  ***

--- eventHandler_Dom/js/event.js
+++ eventHandler_Dom/js/event.js
@@ -16,3 +16,7 @@ const green=document.getElementById('green');
 green.addEventListener('click',function green(){
     document.body.style.backgroundColor='green';
 })
+//addEventListner Finally use ------
+document.getElementById('blue').addEventListener('click', function blue(){
+    document.body.style.backgroundColor='blue';
+})
