26571305b5ff5a7620988abd2342b1956d83e2c8
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-14 15:00:29 +0600

    *** Dom Node-List ***

--- basic dom/dom.js
+++ basic dom/dom.js
@@ -7,9 +7,5 @@ for(const section of sections){
     section.style.borderRadius='5px';
     section.style.backgroundColor='lightgray';
 }
-// const placeContainer=document.getElementById('place-container');
-// placeContainer.style.backgroundColor='yellow';
-// add and remove class in js dom------------
 const placeContainer=document.getElementById('place-container');
- placeContainer.classList.add('text-cenetr');
- placeContainer.classList.remove('text-cenetr');
+console.log(placeContainer.childNodes[1]);
